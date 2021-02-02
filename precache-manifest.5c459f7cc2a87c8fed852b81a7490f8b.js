13c3a6bf4b620c7277c4475fd709ced89389dca7
Usarneme <jamestcoop@gmail.com>
2021-02-02 13:26:09 -0800

    Styleguide-dist npm package used to create build of auto-gen documentation. Checking hosting at gh-pages functionality.

---
+++ precache-manifest.5c459f7cc2a87c8fed852b81a7490f8b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3545a31e8bf64e8e75",
+    "url": "build/2.df3545a3.js"
+  },
+  {
+    "revision": "bc74af22c3cf785a2b164bffefb9cd3d",
+    "url": "build/2.df3545a3.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c7ec5eac7c87c16696",
+    "url": "build/bundle.53c7ec5e.js"
+  },
+  {
+    "revision": "af071474f1da06742df2",
+    "url": "build/main.af071474.js"
+  },
+  {
+    "revision": "54ac74bc41cef911c2a1409a3a20c58d",
+    "url": "index.html"
+  },
+  {
+    "revision": "af071474f1da06742df2",
+    "url": "static/css/main.84a0beb4.chunk.css"
+  },
+  {
+    "revision": "e5a41a1fb16637927922789f450eb11d",
+    "url": "static/media/account.e5a41a1f.svg"
+  },
+  {
+    "revision": "a918556d0b220c9f667051965922a209",
+    "url": "static/media/addLog.a918556d.svg"
+  },
+  {
+    "revision": "0476682c50f4dc09e14eeaa5b1d00482",
+    "url": "static/media/log.0476682c.svg"
+  },
+  {
+    "revision": "369e264bad80bee0321c28b8a4915616",
+    "url": "static/media/magnifying.369e264b.svg"
+  },
+  {
+    "revision": "fb5ad7c73e372f4c2e8119ca41920967",
+    "url": "static/media/search.fb5ad7c7.svg"
+  },
+  {
+    "revision": "91e3307a602423a3a16af1bc8354e577",
+    "url": "static/media/todo.91e3307a.svg"
+  }
+]);
---
+++ precache-manifest.b7e6feb4f09e88f50e63004d6d5cd68b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19c36411c9aa03bbb1a",
+    "url": "build/2.f19c3641.js"
+  },
+  {
+    "revision": "1076d62d5d28cec213d9c1952abf7d16",
+    "url": "build/2.f19c3641.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c7ec5eac7c87c16696",
+    "url": "build/bundle.53c7ec5e.js"
+  },
+  {
+    "revision": "cd21483fa281181ddbc7",
+    "url": "build/main.cd21483f.js"
+  },
+  {
+    "revision": "63aabd1a968605d0fc9edbd9c4fef084",
+    "url": "index.html"
+  },
+  {
+    "revision": "cd21483fa281181ddbc7",
+    "url": "static/css/main.84a0beb4.chunk.css"
+  },
+  {
+    "revision": "e5a41a1fb16637927922789f450eb11d",
+    "url": "static/media/account.e5a41a1f.svg"
+  },
+  {
+    "revision": "a918556d0b220c9f667051965922a209",
+    "url": "static/media/addLog.a918556d.svg"
+  },
+  {
+    "revision": "0476682c50f4dc09e14eeaa5b1d00482",
+    "url": "static/media/log.0476682c.svg"
+  },
+  {
+    "revision": "369e264bad80bee0321c28b8a4915616",
+    "url": "static/media/magnifying.369e264b.svg"
+  },
+  {
+    "revision": "fb5ad7c73e372f4c2e8119ca41920967",
+    "url": "static/media/search.fb5ad7c7.svg"
+  },
+  {
+    "revision": "91e3307a602423a3a16af1bc8354e577",
+    "url": "static/media/todo.91e3307a.svg"
+  }
+]);
